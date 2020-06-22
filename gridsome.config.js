b86d0139d0c875e80df08ddad69303a436635776
Grace Snow <graceaesnow@gmail.com>
2020-06-22 11:05:58 +0100

    Add Google analytics

--- gridsome.config.js
+++ gridsome.config.js
@@ -84,6 +84,12 @@ module.exports = {
           },
         }
       }
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-170308798-1'
+      }
     }
   ],
   transformers: {
