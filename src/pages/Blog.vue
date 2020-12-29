d77e645d7af5c34cee2879a6ea55804333285e91
Grace Snow <grace.snow@mandogroup.com>
2020-12-29 14:58:26 +0000

    give talk section dark bg

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -32,7 +32,7 @@
         <Pager :info="$page.blogs.pageInfo" class="pagination" />
       </section>
 
-      <section class="section page-width page-padding h5Text talks">
+      <section class="section page-width page-padding h5Text talks dark-bg">
         <h2 class="equals-decal">Talks</h2>
         <p>
           I've had the pleasure of speaking at several local meetups, as well as leading countless
@@ -50,13 +50,13 @@
         </p>
       </section>
     </div>
-    <Contact class="page-header border-top" />
+    <Contact class="page-header" />
   </Layout>
 </template>
 
 <page-query>
   query blogPosts($page: Int) {
-    blogs: allBlogPost (filter: { blogpost: { eq: true }}, sortBy: "date", order: DESC, perPage: 6, page: $page) @paginate {
+    blogs: allBlogPost (filter: { blogpost: { eq: true }, published: { eq: true }}, sortBy: "date", order: DESC, perPage: 6, page: $page) @paginate {
       pageInfo {
         totalPages
         currentPage
@@ -146,7 +146,12 @@ export default {
 }
 
 .blog-listing__item {
-  margin-bottom: 2.5em;
+  margin-bottom: 2rem;
+
+  &:last-of-type,
+  &:only-of-type {
+    margin-bottom: 0;
+  }
 }
 
 .blog-listing__link {
@@ -172,12 +177,75 @@ export default {
   @include small-caps-title;
 }
 
+/* Talks section*/
+.talks {
+  position: relative;
+  /* background: $intro-gradient; */
+  background-color: rgba(#00375c, 20%);
+
+  &:before,
+  &:after {
+    position: absolute;
+    content: '';
+    height: 100%;
+    top: 0;
+    right: 0;
+  }
+
+  &:before {
+    width: 100%;
+    height: 100%;
+    opacity: 0.8;
+    z-index: -1;
+    background-image: $intro-gradient;
+  }
+
+  &:after {
+    /* &:before { */
+    width: 100%;
+    background: $neutral-150;
+    mix-blend-mode: color-burn;
+    clip-path: polygon(0px 65%, 65% 100%, 100% 100%, 100% 0);
+    z-index: 0;
+    @include media-up(small) {
+      clip-path: polygon(100% 0, 68vw 100%, 100% 100%);
+    }
+  }
+}
+
 .talks__subtitle {
   margin-top: 2.5rem;
 }
 
+.dark-bg {
+  p,
+  ul,
+  ol {
+    color: #fff;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    color: $blue-200;
+  }
+
+  .link {
+    color: $accent-200;
+    transition: color 0s ease-in 0.2s;
+    white-space: nowrap;
+
+    &:hover {
+      color: $blue-1000;
+    }
+  }
+}
+
 /* Pagination styles */
 .pagination {
+  margin-top: 2rem;
   display: flex;
   align-items: center;
   line-height: 1;
@@ -206,8 +274,9 @@ export default {
 
   &.active {
     color: $neutral-50;
+
     &:before {
-      background-color: $blue-600; 
+      background-color: $blue-600;
     }
   }
 }
